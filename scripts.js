e0deed748967e68826c31d3ce54c27b73fbaed8c
Monse566 <alejandramvc666@gmail.com>
2024-02-11 21:17:27 -0600

    fin video

--- scripts.js
+++ scripts.js
@@ -1,7 +1,26 @@
 const person = {
   name: "Juan",
   age: 26,
-  sons: ["Laura", "Diego"],
+  sons: ["Laura", "Diego", "Pedro", "Rosa", "Tomas"],
 };
 
-console.log("person");
+/*console.log(person.age);
+console.log(person["sons"]);
+
+for (const key in person) {
+  console.log(key);
+}
+
+for (const key in person) {
+  console.log(person[key]);
+}
+
+for (const son of person.sons) {
+  console.log(son);
+}*/
+
+console.log(
+  `Hola ${person.name} tienes ${
+    person.age
+  } años y tus hijos se llaman ${person.sons.join(",")}`
+);
