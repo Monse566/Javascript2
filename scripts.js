36bf78599be76cf823b75b834e61d25aeaad93c1
Monse566 <alejandramvc666@gmail.com>
2024-03-22 20:32:45 -0600

    final

--- scripts.js
+++ scripts.js
@@ -52,20 +52,25 @@ let result = suma(3, 6);
 
 console.log(result);*/
 
-class Persona {
+/*class Persona {
   constructor(nombre, apellido, edad) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.edad = edad;
 
-    this.datos =
-      "Me llamo ${nombre} ${this.apellido} y tengp ${this.edad} años";
+    this.datos = `Me llamo ${nombre} ${this.apellido} y tengo ${this.edad} años`;
   }
 
   saludar() {
-    return "Hola, me llamo ${this.nombre} y tengo ${this.edad}";
+    return `Hola, me llamo ${this.nombre} y tengo ${this.edad}`;
   }
 }
 
 const Juan = new Persona("Juan", "Garcia", 25);
 const Marta = new Persona("Marta", "Garcia", 35);
+
+console.log(Juan);
+
+console.log(Juan.saludar());
+
+console.log(Marta.saludar());*/
